92e0d9401fe1604bd3c207c4b7f61f9a6f658902
masyaqze <maksimstepaniak345@gmail.com>
2025-06-11 18:54:25 +0300

    функція winner

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,18 @@
 # Hit, Deal, стратегія комп’ютера
+
+from score import calculate_score
+
+def winner(player_hand, dealer_hand):
+    player_score = calculate_score(player_hand)
+    dealer_score = calculate_score(dealer_hand)
+
+    if player_score > 21:
+        return "Ти програв! Перебір. "
+    elif dealer_score > 21:
+        return "Ти виграв! Дилер перебрав. "
+    elif player_score == dealer_score:
+        return "Нічия! "
+    elif player_score > dealer_score:
+        return "Ти виграв! "
+    else:
+        return "Ти програв! "
