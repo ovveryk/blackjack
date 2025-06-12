aaa0a4605e072c6d3a3f91cfe73fdd28f060fc55
masyaqze <maksimstepaniak345@gmail.com>
2025-06-12 13:19:44 +0300

    added dealer card function

---
+++ dealer.py
@@ -0,0 +1,21 @@
+from cards import deck
+from score import calculate_score
+
+def dealer_hand():
+    dealer_cards = deck[:2]
+    counter = 2
+    game_over = False
+    score = calculate_score(dealer_cards)
+
+    if len(dealer_cards) == 2 and score == 22 or score == 21:
+        return dealer_cards
+
+    while not game_over:
+        if score < 17:
+            counter += 1
+            dealer_cards.append(deck[counter])
+            score = calculate_score(dealer_cards)
+        else:
+            game_over = True
+
+    return dealer_cards
