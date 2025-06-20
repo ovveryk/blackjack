3ca4c1bf5c8127238589658cfa11c02ea5eef758
masyaqze <maksimstepaniak345@gmail.com>
2025-06-20 13:04:44 +0300

    update score.py

--- score.py
+++ score.py
@@ -3,17 +3,13 @@ from constants import CARD_NAMES
 
 def calculate_score(hand):
 
-
     calc = 0
     aces = 0
 
     for card in hand:
-        for suit in ['\u2660', '\u2665', '\u2666', '\u2663']:
-            card = card.replace(suit, '')
-
-        calc += CARD_NAMES.get(card, 0)
-
-        if card == 'A':
+        value = CARD_NAMES.get(card, 0)
+        calc += value
+        if value == 11:
             aces += 1
 
     while calc > 21 and aces > 0:
